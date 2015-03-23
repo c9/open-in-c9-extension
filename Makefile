238774f311af47595fc7597f894c8797b479eae5
Stuart P. Bentley <stuart@testtrack4.com>
2015-03-23 10:02:12 +0000

    Inject stylesheet in script

--- Makefile
+++ Makefile
@@ -1,15 +1,14 @@
-INPUT = manifest.json githubc9.js styles.css c9-16.png c9-48.png c9-128.png
+INPUT = manifest.json githubc9.js c9-16.png c9-48.png c9-128.png
 
 all: open-in-c9.zip
 
 open-in-c9: $(INPUT)
 	mkdir -p $@
 	cp $^ $@
-	
+
 open-in-c9.zip: open-in-c9 $(INPUT)
 	zip -r $@ open-in-c9
-	
+
 clean:
 	rm -rf open-in-c9.zip open-in-c9
-	
-	
+
--- githubc9.js
+++ githubc9.js
@@ -1,10 +1,10 @@
 (function () {
-    
+
 var defaultBgImage = "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAALCAYAAAByF90EAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAZpJREFUeNp8kr9Lw0AUx5ukTdMfaWvALlKyKKWNGBBxkIjUSagoKbQ4Z01HcayD0MFJcChF7f9Q6lJQQRycpLM4tC4luhVd1Kbn90ojJUYPPlzu7r1vvvfeMYQQ3x9DBPJk/gAvoA+8E6iQByJYKxaLeiAQuGEYZhCNRh/T6fQRUjivHPcGDwSglkolHQJvEDpF8gaEzFAo1Adn/wnR5CWw3mq1dhuNRikYDN5D6AR7i7lcbp7GVavVAsdxtmmaBaznADMtRBcrNDmZTHb8fj9xYFk2jzMJrq4hmsX3ajwe79K4VCp1q6rqMYQ5R2jGsqzNRCLxzPM8mSYcDhuTZuQzmcwydRyLxazpGFEUrxyhWcMw9nEV4kaSpG6tVlMm9hfq9fqeVxxMbY0LrOv6IYpIvBAEgcDtAeI0RVGaXjEoQZmF7U9ZlpvD4fALdfC5wR+f2u12p1KpZHu93rb73LZt32g0evhpH+5eph2hDiKRCEG7x8DNu6ZpF5gHzh6FOqENgZHzX+8IIwsuwevkBf8Ffel3YMfJ/RZgALTrMYKs/GM2AAAAAElFTkSuQmCC)";
 
 
-    
-// new layout     
+
+// new layout
 var target = document.getElementsByClassName("clone-options")[0];
 if (target)
     newLayout(target);
@@ -12,7 +12,7 @@ else
     oldLayout();
 
 
-function oldLayout() {    
+function oldLayout() {
     // this is just github!
     if (!document.getElementsByClassName("native-clones").length || !document.getElementsByClassName("public_clone_url").length)
     	return;
@@ -79,13 +79,21 @@ function oldLayout() {
 
     	return true;
     });
-    
+
     // and add it to the DOM
     target.parentNode.insertBefore(li, target);
 }
 
 
 function newLayout(target) {
+    var style = document.createElement("style");
+    style.textContent = ".c9-button>span {" +
+        "background-repeat: no-repeat;" +
+        "background-position: left 4px;" +
+        "padding: 2px 0 0 26px;" +
+        "background-image: " + defaultBgImage + ";}";
+    document.head.appendChild(style);
+
     var link = document.createElement("a");
     link.className = "minibutton sidebar-button c9-button";
     link.title = "Open this repository in Cloud9";
@@ -95,19 +103,19 @@ function newLayout(target) {
     var isPrivate = document.getElementsByClassName("private").length > 0;
     var user = document.querySelectorAll(".author>a>span")[0].textContent;
     var repo = document.querySelectorAll(".js-current-repository")[0].textContent;
-    
+
     if (isPrivate)
         var url = "git@github.com:" + user + "/" + repo + ".git";
     else
         var url = "git://github.com/" + user + "/" + repo + ".git";
-        
+
     link.href = "https://c9.io/open/git/?url=" + url;
-    
+
     var icon = document.createElement("span");
-    
+
     link.appendChild(icon);
     link.appendChild(document.createTextNode("Clone in Cloud9"));
-    
+
     target.parentNode.insertBefore(link, target.nextSibling);
 }
 
