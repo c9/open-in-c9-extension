24c0315f75b076d2ead2f179893de8693e907f23
Fabian Jakobs <fabian.jakobs@web.de>
2015-07-15 11:27:50 +0200

    cleanup

--- githubc9.js
+++ githubc9.js
@@ -1,7 +1,5 @@
 (function () {
     
-var defaultBgImage = "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAALCAYAAAByF90EAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAZpJREFUeNp8kr9Lw0AUx5ukTdMfaWvALlKyKKWNGBBxkIjUSagoKbQ4Z01HcayD0MFJcChF7f9Q6lJQQRycpLM4tC4luhVd1Kbn90ojJUYPPlzu7r1vvvfeMYQQ3x9DBPJk/gAvoA+8E6iQByJYKxaLeiAQuGEYZhCNRh/T6fQRUjivHPcGDwSglkolHQJvEDpF8gaEzFAo1Adn/wnR5CWw3mq1dhuNRikYDN5D6AR7i7lcbp7GVavVAsdxtmmaBaznADMtRBcrNDmZTHb8fj9xYFk2jzMJrq4hmsX3ajwe79K4VCp1q6rqMYQ5R2jGsqzNRCLxzPM8mSYcDhuTZuQzmcwydRyLxazpGFEUrxyhWcMw9nEV4kaSpG6tVlMm9hfq9fqeVxxMbY0LrOv6IYpIvBAEgcDtAeI0RVGaXjEoQZmF7U9ZlpvD4fALdfC5wR+f2u12p1KpZHu93rb73LZt32g0evhpH+5eph2hDiKRCEG7x8DNu6ZpF5gHzh6FOqENgZHzX+8IIwsuwevkBf8Ffel3YMfJ/RZgALTrMYKs/GM2AAAAAElFTkSuQmCC)";
-  
 // new layout     
 var target = document.getElementsByClassName("js-clone-url")[0];
 newLayout(target);
@@ -17,10 +15,11 @@ function newLayout(target) {
     var user = document.querySelectorAll(".author>a>span")[0].textContent;
     var repo = document.querySelectorAll(".entry-title>strong>a")[0].textContent;
     
+    var url;
     if (isPrivate)
-        var url = "git@github.com:" + user + "/" + repo + ".git";
+        url = "git@github.com:" + user + "/" + repo + ".git";
     else
-        var url = "git://github.com/" + user + "/" + repo + ".git";
+        url = "git://github.com/" + user + "/" + repo + ".git";
         
     link.href = "https://c9.io/open/git/?url=" + url;
     
